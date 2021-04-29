a2a30fcca3506ed790123bdc1e0146c53aca0964
WibyFabian08 <wibyfabian08@gmail.com>
2021-04-29 14:39:00 -0700

    Initial Commit

---
+++ pages/haloo.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <div className="text-center container mt-5">
+        <Head>
+            <title>BWAMICRO</title>
+        </Head>
+      <main>
+        <h1>Halooooooo</h1>
+      </main>
+    </div>
+  )
+}
